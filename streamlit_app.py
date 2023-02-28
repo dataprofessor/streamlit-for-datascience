c66a3d386a003990bcd5f338d4ed48691fc30054
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-02-28 20:27:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,10 @@ lesson_list = [f'Lesson {x}' for x in md_files]
 
 query_params = st.experimental_get_query_params()
 
-#if query_params and query_params["challenge"][0] in days_list:
-#    st.session_state.day = query_params["challenge"][0]
+if query_params and query_params["lesson"][0] in lesson_list:
+    st.session_state.day = query_params["lesson"][0]
 
-#selected_day = st.selectbox('Start the Challenge 👇', days_list, key="day", on_change=update_params)
+#selected_day = st.selectbox('Select a Lesson 👇', lesson_list, key="lesson", on_change=update_params)
 
 
 st.write(md_files)
