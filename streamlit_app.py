b39728b0aa1507a8c71fd546981dd11eeeddb1d2
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-02-28 22:54:13 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ import base64
 from pathlib import Path
 from utilities import load_bootstrap
 
+# Image display
 load_bootstrap()
-
 def img_to_bytes(img_path):
     img_bytes = Path(img_path).read_bytes()
     encoded = base64.b64encode(img_bytes).decode()
