dfb670ff6abd41b51d3d73a333ff1d614f6b05e9
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-02-28 23:01:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,5 +63,8 @@ img_html = '''<br>
 #st.image('./img/lesson-2-data-science-life-cycle.png')
 
 img_url = './img/lesson-2-data-science-life-cycle.png'
+img_html = '<img src="./img/lesson-2-data-science-life-cycle.png" width="60%">'
 
 st.markdown(img_to_html(img_url), unsafe_allow_html=True)
+
+st.markdown(img_to_html(img_html.replace('<img src=','').replace('>', '') ), unsafe_allow_html=True)
