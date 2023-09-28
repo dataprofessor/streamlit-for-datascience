ab9e758fc7fc8f8782195ff138599a8b18c562fb
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-28 10:41:54 +0700

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import requests
 
-def load_bootstrap():
-    return st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+def load_css():
+    with open("style.css") as f:
+        st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+
+def read_md(md_file):
+  img_url_path = 'https://github.com/dataprofessor/streamlit-for-datascience/blob/master'
+  img_url_suffix = '?raw=true'
+  
+  md_url_path = 'https://raw.githubusercontent.com/dataprofessor/streamlit-for-datascience/master/content'
+  md_lesson_url = f'{md_url_path}/{md_file}'
+  response = requests.get(md_lesson_url)
+  content = response.text
+  content = content.replace('<img src="..', f'<img src="{img_url_path}').replace('" width', f'{img_url_suffix}" width')
+  return content
