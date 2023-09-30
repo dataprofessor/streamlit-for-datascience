9809dfe2abb264babb517060478e31c60ef1fdaf
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-09-30 16:09:05 +0000

    Create 5_5_Data_visualization_with_Matplotlib.py

---
+++ pages/5_5_Data_visualization_with_Matplotlib.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+from utilities import load_css, read_md
+
+st.set_page_config(page_title="Lesson 5 - Data Visualization with Matplotlib", page_icon="📖", layout="wide")
+
+load_css()
+
+md = read_md('Lesson-4.md')
+st.markdown(md, unsafe_allow_html=True)
