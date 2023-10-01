7a2ab65d7ecb20caede64a24c5e2beaf5b54b16c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-10-01 02:44:55 +0000

    Update and rename 7_Project_1_NumPy_in_Streamlit to 7_Project_1_NumPy_in_Streamlit.py

---
+++ pages/7_Project_1_NumPy_in_Streamlit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+from utilities import load_css, read_md
+
+st.set_page_config(page_title="Project 1 - Using NumPy in Streamlit", page_icon="📖", layout="wide")
+
+load_css()
+
+md = read_md('Lesson-7.md')
+st.markdown(md, unsafe_allow_html=True)
