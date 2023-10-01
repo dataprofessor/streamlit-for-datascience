fddca5d6345d5d8b1932b1e1ab4a4b51edcc6b08
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-10-01 03:03:48 +0000

    Create 11_Project_5_Scikit-learn_in_Streamlit.py

---
+++ pages/11_Project_5_Scikit-learn_in_Streamlit.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+from utilities import load_css, read_md
+
+st.set_page_config(page_title="Project 5 - Using Scikit-learn to create an ML model in Streamlit", page_icon="🛠️", layout="wide")
+
+load_css()
+
+md = read_md('Project-5.md')
+st.markdown(md, unsafe_allow_html=True)
